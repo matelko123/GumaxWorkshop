06aeec64523a118c6448a6eb5104871c8fabb8c1
matelko <mateusz280798@gmail.com>
2023-06-06 22:07:03 +0200

    Add Client configuration

---
+++ src/GumaxWorkshop.Infrastructure/Configuration/ClientConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using GumaxWorkshop.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GumaxWorkshop.Infrastructure.Configuration;
+
+public class ClientConfiguration : IEntityTypeConfiguration<Client>
+{
+    public void Configure(EntityTypeBuilder<Client> builder)
+    {
+        builder.ToTable("Clients");
+        
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.FirstName)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(c => c.LastName)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(c => c.Address)
+            .HasMaxLength(100);
+
+        builder.Property(c => c.PhoneNumber)
+            .IsRequired()
+            .HasMaxLength(15);
+    }
+}
