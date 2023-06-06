ef6f758352a7aeeeec4536dada20fdfe74962f5d
matelko <mateusz280798@gmail.com>
2023-06-06 22:03:35 +0200

    Add ApplicationDbContext

---
+++ src/GumaxWorkshop.Application/Data/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using GumaxWorkshop.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GumaxWorkshop.Application.Data;
+
+public interface IApplicationDbContext
+{
+    public DbSet<Client> Clients { get; set; }
+    
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
---
+++ src/GumaxWorkshop.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using GumaxWorkshop.Application.Data;
+using GumaxWorkshop.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GumaxWorkshop.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public DbSet<Client> Clients { get; set; }
+    
+    public ApplicationDbContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
