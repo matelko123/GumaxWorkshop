bd2ef7a801d4bdd94decb380b50768a36191e387
matelko <mateusz280798@gmail.com>
2023-06-06 22:03:28 +0200

    Add Client entity

---
+++ src/GumaxWorkshop.Domain/Entities/Client.cs
@@ -0,0 +1,20 @@
+﻿using GumaxWorkshop.Domain.ValueObjects;
+
+namespace GumaxWorkshop.Domain.Entities;
+
+public class Client
+{
+    public required Guid Id { get; init; }
+    public required ClientType Type { get; set; } = ClientType.Individual;
+    public required string PhoneNumber { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string CompanyName { get; set; } = string.Empty;
+    public NIP? NIP { get; set; }
+}
+public enum ClientType
+{
+    Individual,
+    Company
+}
---
+++ src/GumaxWorkshop.Domain/ValueObjects/NIP.cs
@@ -0,0 +1,42 @@
+﻿namespace GumaxWorkshop.Domain.ValueObjects;
+
+public class NIP
+{
+    private readonly string _value;
+
+    public NIP(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException("NIP cannot be empty.", nameof(value));
+
+        if (!IsValidNIP(value))
+            throw new ArgumentException("Invalid NIP format.", nameof(value));
+
+        _value = value;
+    }
+
+    public override string ToString() => _value;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is NIP otherNIP)
+        {
+            return _value == otherNIP._value;
+        }
+
+        return false;
+    }
+
+    protected bool Equals(NIP other)
+    {
+        return _value == other._value;
+    }
+
+    public override int GetHashCode()
+    {
+        return _value.GetHashCode();
+    }
+
+    private bool IsValidNIP(string value) 
+        => value.Length == 10 && value.All(char.IsDigit);
+}
