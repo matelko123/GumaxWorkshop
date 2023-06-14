c26ce31ed7ea29cfddd9650b870f1b540b0cbbbb
matelko <mateusz280798@gmail.com>
2023-06-14 19:41:10 +0200

    Change to record

--- src/GumaxWorkshop.Application/Clients/Commands/Create/CreateClientCommand.cs
+++ src/GumaxWorkshop.Application/Clients/Commands/Create/CreateClientCommand.cs
@@ -3,13 +3,12 @@ using MediatR;
 
 namespace GumaxWorkshop.Application.Clients.Commands.Create;
 
-public sealed class CreateClientCommand : IRequest<CreateClientResponse>
-{
-    public ClientType Type { get; set; }
-    public string PhoneNumber { get; set; }
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
-    public string? Address { get; set; }
-    public string? CompanyName { get; set; }
-    public string? NIP { get; set; }
-}
+public sealed record CreateClientCommand(
+    ClientType Type,
+    string PhoneNumber,
+    string? FirstName,
+    string? LastName,
+    string? Address,
+    string? CompanyName,
+    string? NIP
+) : IRequest<CreateClientResponse>;
