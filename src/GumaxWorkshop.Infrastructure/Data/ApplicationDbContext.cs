422df8e79606be07c847773901f20300c71ad1e5
matelko <mateusz280798@gmail.com>
2023-06-14 19:34:52 +0200

    Fix warning

--- src/GumaxWorkshop.Infrastructure/Data/ApplicationDbContext.cs
+++ src/GumaxWorkshop.Infrastructure/Data/ApplicationDbContext.cs
@@ -6,8 +6,8 @@ namespace GumaxWorkshop.Infrastructure.Data;
 
 public class ApplicationDbContext : DbContext, IApplicationDbContext
 {
-    public DbSet<Client> Clients { get; set; }
-    
+    public DbSet<Client> Clients { get; set; } = null!;
+
     public ApplicationDbContext(DbContextOptions options)
         : base(options)
     {
